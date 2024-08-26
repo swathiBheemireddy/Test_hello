7dd5c948e659b9c9054c4c1e65571ba404efd2dc
swathi <bheemireddyswathi2@gmail.com>
2024-08-25 23:48:05 -0700

    test script change

--- src/test_script.py
+++ src/test_script.py
@@ -1,26 +1,55 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-import time
 from selenium.webdriver.chrome.options import Options
+import time
 import os
 
 # Set up the ChromeDriver service and options
-service = Service(executable_path="C:\\swathi_online training\\JarFiles\\Chromedriver\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe")
+service = Service(executable_path=os.path.join("C:", "swathi_online training", "JarFiles", "Chromedriver", "chromedriver-win64", "chromedriver-win64", "chromedriver.exe"))
 options = webdriver.ChromeOptions()
-driver = webdriver.Chrome(service=service, options=options)
-# Open the temporary HTML file in the browser
-driver.get("file://" + os.path.abspath("hello.html"))
 
-# Maximize the browser window
-driver.maximize_window()
-time.sleep(5)
+try:
+    driver = webdriver.Chrome(service=service, options=options)
 
-# Print "hello world!" to the console
-print("hello world!")
-print("venkat")
+    # Define your variables
+    n1 = "Hello"
+    n2 = "World"
 
-# Close the driver
-driver.quit()
+    # Create the HTML content
+    html_content = f"""
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>hello world</title>
+    </head>
+    <body>
+    <h1>{n1} <br> {n2}</h1>Hello World!!!!Welcome Swathi
 
+    <p> {n1} <br> </p>
+    </body>
+    </html>
+    """
 
-# i guess i got it
+    # Write the HTML content to a temporary file
+    temp_file_path = os.path.abspath("temp_hello_world.html")
+    with open(temp_file_path, "w") as file:
+        file.write(html_content)
+
+    # Open the temporary HTML file in the browser
+    driver.get("file://" + temp_file_path)
+
+    # Maximize the browser window
+    driver.maximize_window()
+    time.sleep(15)
+
+    # Print "hello world!" to the console
+    print("hello world!")
+
+finally:
+    # Ensure the driver is closed and the file is removed
+    if 'driver' in locals():
+        driver.quit()
+
+    if os.path.exists(temp_file_path):
+        os.remove(temp_file_path)
